a162af5cd1237524182310ffc5add2f844e8e2b0
Alex Varga <alexvarga09072001@gmail.com>
2021-09-16 23:04:44 +0300

    change vue.config.js and add dist

---
+++ precache-manifest.2ddd03eb109673b2febb4bf67936c11c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5587ace3b51fcda4fa49",
+    "url": "/test_thefirstthelast/css/app.be27ee63.css"
+  },
+  {
+    "revision": "55529b248297a63ba84e516608241db3",
+    "url": "/test_thefirstthelast/fonts/HelveticaRegular.55529b24.ttf"
+  },
+  {
+    "revision": "63f330cb9009c77157839fd8062b050d",
+    "url": "/test_thefirstthelast/img/BeruWhitesunlars.63f330cb.jpg"
+  },
+  {
+    "revision": "f942767ba9c425016f2f758a5b4468da",
+    "url": "/test_thefirstthelast/img/BiggsDarklighter.f942767b.jpg"
+  },
+  {
+    "revision": "68d75aa6250b58eaf5bc204afec010aa",
+    "url": "/test_thefirstthelast/img/C-3PO.68d75aa6.png"
+  },
+  {
+    "revision": "de86594ad894fcaf0c383772f5608e89",
+    "url": "/test_thefirstthelast/img/DarthVader.de86594a.jpg"
+  },
+  {
+    "revision": "99ffce79360d9731ec6f26e75146c0b1",
+    "url": "/test_thefirstthelast/img/LeiaOrgana.99ffce79.jpg"
+  },
+  {
+    "revision": "8282636e04746ff56b621fa433c0b3db",
+    "url": "/test_thefirstthelast/img/LukeSkywalker.8282636e.jpg"
+  },
+  {
+    "revision": "678c21beeac6ff7d203b9ebcb5ea3583",
+    "url": "/test_thefirstthelast/img/Obi-WanKenobi.678c21be.jpg"
+  },
+  {
+    "revision": "bb5a762c74ed31c8e5096dbfb05bff6c",
+    "url": "/test_thefirstthelast/img/OwenLars.bb5a762c.jpg"
+  },
+  {
+    "revision": "7ecc0358a52ac79561c98ebf7031ab9d",
+    "url": "/test_thefirstthelast/img/R2-D2.7ecc0358.jpg"
+  },
+  {
+    "revision": "de051890dd053b3e42b522358dab750a",
+    "url": "/test_thefirstthelast/img/R5-D4.de051890.jpeg"
+  },
+  {
+    "revision": "1ba6f69da0e62d695f38a4a923ddc2a2",
+    "url": "/test_thefirstthelast/img/down.1ba6f69d.svg"
+  },
+  {
+    "revision": "67cc269988078b26f632a57fc6d9cf62",
+    "url": "/test_thefirstthelast/img/facebook.67cc2699.svg"
+  },
+  {
+    "revision": "9030c8d867d36cc049b36eb2ea54997a",
+    "url": "/test_thefirstthelast/img/instagram.9030c8d8.svg"
+  },
+  {
+    "revision": "41a75f6ce9bf1af8579c04f72014b166",
+    "url": "/test_thefirstthelast/index.html"
+  },
+  {
+    "revision": "5587ace3b51fcda4fa49",
+    "url": "/test_thefirstthelast/js/app.f60a1704.js"
+  },
+  {
+    "revision": "6fd629dea7c7e64c2e1c",
+    "url": "/test_thefirstthelast/js/chunk-vendors.3c029586.js"
+  },
+  {
+    "revision": "af7e842fc0a686ea8ae1a965ff0f5cdb",
+    "url": "/test_thefirstthelast/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/test_thefirstthelast/robots.txt"
+  }
+]);
